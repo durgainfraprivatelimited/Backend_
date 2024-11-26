408bee6213ba52904167c5538b0f588daff3eea1
durgainfraprivatelimited <durgainfraprivatelimited@gmail.com>
2024-11-26 08:08:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 
 // Remove the previous middleware that set '*' and replace with specific origin
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://final-brown-six.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
