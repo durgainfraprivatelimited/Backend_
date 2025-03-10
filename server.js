518543b433efac0c6ba1a60ad4533f914857121a
durgainfraprivatelimited <durgainfraprivatelimited@gmail.com>
2025-03-10 21:15:40 +0530

    Update server.js final

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Specific origin instead of wildcard
 const corsOptions = {
-  origin: ['https://shrivagdeviconstructions.com','https://shrivagdeviconstructions.vercel.app'],// Both production and development URLs
+  origin: ['https://www.shrivagdeviconstructions.com/','https://shrivagdeviconstructions.vercel.app'],// Both production and development URLs
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
